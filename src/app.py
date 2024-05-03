4348859f718daff33c8c25d18e12c94b2275a6af
Leo Shen <shen@Leoshen>
2024-05-03 13:17:25 +0800

    able to ask question via cmd line; remove two columns

--- src/app.py
+++ src/app.py
@@ -7,6 +7,44 @@ from langchain_core.prompts import ChatPromptTemplate
 
 from streamlit_float import *
 
+from langchain.agents import create_tool_calling_agent
+from langchain.agents import AgentExecutor
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain.agents import AgentType, initialize_agent, load_tools
+
+user_story_template = """
+You are a business analyst who is familiar with specification by example. I’m the domain expert.
+// 你是一个业务分析师，而我是领域专家 
+
+===CONTEXT
+{context}
+===END OF CONTEXT
+
+===USER STORY
+{story}
+===END OF USER STORY 
+
+Explain the user story as scenarios. Use the following format:
+// 使用 场景 解释用户故事，并遵循如下格式 
+
+Thought: you should always think about what is still uncertain about the user story. Ignore technical concerns.
+// 思考：你应该考虑用户故事中不清晰的部分。但忽略技术细节
+Question: the question to ask to clarify the user story
+// 问题：提出问题帮助你澄清这个用户故事
+Answer: the answer I responded to the question
+// 回答：我给出答案
+… (this Thought/Question/Answer repeat at least 3 times, at most 10 times)
+//（Thought/Question/Answer 重复至少 3 次而不多于 10 次）
+Thought: I know enough to explain the user story
+// 思考：我已经对这个用户故事了解了足够多的内容
+Scenarios: List all possible scenarios with concrete example in Given/When/Then style
+// 场景：列出所有场景。使用 Given/When/Then 的格式表述
+
+{history}
+{input}
+请使用中文
+"""
 
 # app config
 st.set_page_config(page_title="Streaming bot", page_icon="🤖", layout="wide")
@@ -15,27 +53,29 @@ st.title("Streaming bot")
 float_init(theme=True, include_unstable_primary=False)
 
 load_dotenv()
+from langchain_community.tools import HumanInputRun
 
-def get_response(user_query, chat_history):
+def get_response(user_query, chat_history, user_story, business_ctx):
+  
+    llm = ChatOpenAI(temperature=0.0, model="gpt-4-turbo-preview")
+    tools = load_tools(["human"])
 
-    template = """
-    You are a helpful assistant. Answer the following questions considering the history of the conversation:
-
-    Chat history: {chat_history}
-
-    User question: {user_question}
-    """
-
-    prompt = ChatPromptTemplate.from_template(template)
-
-    llm = ChatOpenAI()
-        
-    chain = prompt | llm | StrOutputParser()
-    
-    return chain.stream({
-        "chat_history": chat_history,
-        "user_question": user_query,
-    })
+    agent_chain = initialize_agent(
+        tools,
+        llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
+    )
+    prompt = ChatPromptTemplate.from_template(user_story_template)
+    prompt_value = prompt.invoke(
+        {
+            "history": chat_history,
+            "input": user_query,
+            "story": user_story,
+            "context": business_ctx,
+        }
+    )
+    return agent_chain.invoke(prompt_value)
 
 left_column, right_column = st.columns(2)
 
@@ -55,49 +95,49 @@ if "chat_history" not in st.session_state:
 else:
     border = True
 
-with left_column:    
-    with st.container(border=border, height=800):
-        # conversation
-        for message in st.session_state.chat_history:
-            if isinstance(message, AIMessage):
-                with st.chat_message("AI"):
-                    st.write(message.content)
-            elif isinstance(message, HumanMessage):
-                with st.chat_message("Human"):
-                    st.write(message.content)
+# with right_column:
+user_story = st.text_area(
+    "User Story",
+    """作为学校的教职员工（As a faculty），
+    我希望学生可以根据录取通知将学籍注册到教学计划上（I want the student to be able to enroll in an academic program with given offer），
+    从而我可以跟踪他们的获取学位的进度（So that I can track their progress）""",
+    height= 300,
+)
 
-        # user input
-        user_query = ''
-        with st.container():
-            user_query = st.chat_input("What is up?")
-            button_b_pos = "0rem"
-            button_css = float_css_helper(width="2.2rem", bottom=button_b_pos, transition=0)
-            float_parent(css=button_css)
+business_ctx = st.text_area(
+    "Business Context",
+    "整个学籍管理系统是一个 Web 应用； 当教职员工发放录取通知时，会同步建立学生的账号；学生可以根据身份信息，查询自己的账号；在报道注册时，学生登录账号，按照录取通知书完成学年的注册；",
+    height= 300,
+)
 
-        if user_query is not None and user_query != "":
-            st.session_state.chat_history.append(HumanMessage(content=user_query))
-
-            with st.chat_message("Human"):
-                st.markdown(user_query)
-
+# with left_column:    
+with st.container(border=border, height=800):
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
             with st.chat_message("AI"):
-                response = st.write_stream(get_response(user_query, st.session_state.chat_history))
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
 
-            st.session_state.chat_history.append(AIMessage(content=response))
+    # user input
+    user_query = ''
+    with st.container():
+        user_query = st.chat_input("What is up?")
+        button_b_pos = "0rem"
+        button_css = float_css_helper(width="2.2rem", bottom=button_b_pos, transition=0)
+        float_parent(css=button_css)
 
-with right_column:
-    user_story = st.text_area(
-        "User Story",
-        "...",
-        height= 300,
-    )
+    if user_query is not None and user_query != "":
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
 
-    business_ctx = st.text_area(
-        "Business Context",
-        "It was the best of times, it was the worst of times, it was the age of "
-        "wisdom, it was the age of foolishness, it was the epoch of belief, it "
-        "was the epoch of incredulity, it was the season of Light, it was the "
-        "season of Darkness, it was the spring of hope, it was the winter of "
-        "despair, (...)",
-        height= 300,
-    )
+        with st.chat_message("Human"):
+            st.markdown(user_query)
+
+        with st.chat_message("AI"):
+            response = st.write_stream(get_response(user_query, st.session_state.chat_history, user_story, business_ctx))
+
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+

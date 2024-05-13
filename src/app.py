1277ede7e486cebe7817e2ab9c426cd76d3ef1cd
linwencai <hiac@vip.qq.com>
2024-05-13 19:42:38 +0800

    fix: interactive checkbox

--- src/app.py
+++ src/app.py
@@ -65,10 +65,17 @@ load_dotenv()
 database.init_database()
 
 
-def get_response(user_query, chat_history, user_story, business_ctx, is_interactive = True):
+def get_response(user_query, chat_history, user_story, business_ctx, is_interactive=True):
     if "DASHSCOPE_API_KEY" in os.environ:
         llm_chat = Tongyi
-        llm_model_name = "qwen1.5-110b-chat"
+        llm_model_name = "qwen1.5-0.5b-chat"  # 通义千问1.5对外开源的0.5B规模参数量是经过人类指令对齐的chat模型
+        # llm_model_name = "qwen1.5-110b-chat"  # 通义千问1.5对外开源的110B规模参数量是经过人类指令对齐的chat模型
+        # llm_model_name = "baichuan-7b-v1"  # 由百川智能开发的一个开源的大规模预训练模型，70亿参数，支持中英双语，上下文窗口长度为4096。
+        # llm_model_name = "baichuan2-13b-chat-v1"  # 由百川智能开发的一个开源的大规模预训练模型，130亿参数，支持中英双语，上下文窗口长度为4096。
+        # llm_model_name = "llama3-8b-instruct"  # Llama3系列是Meta在2024年4月18日公开发布的大型语言模型（LLMs），llama3-8B拥有80亿参数，模型最大输入为6500，最大输出为1500，仅支持message格式，限时免费调用。
+        # llm_model_name = "ziya-llama-13b-v1"  # 姜子牙通用大模型由IDEA研究院认知计算与自然语言研究中心主导开源，具备翻译、编程、文本分类、信息抽取、摘要、文案生成、常识问答和数学计算等能力。
+        # llm_model_name = "chatyuan-large-v2"  # ChatYuan模型是由元语智能出品的大规模语言模型，它在灵积平台上的模型名称为"chatyuan-large-v2"。ChatYuan-large-v2是一个支持中英双语的功能型对话语言大模型，是继ChatYuan系列中ChatYuan-large-v1开源后的又一个开源模型。
+
     # elif "OPENAI_API_KEY" in os.environ:
     else:
         llm_chat = ChatOpenAI
@@ -116,8 +123,8 @@ with right_column:
     # with tab_user_story:
     user_story, business_ctx = view.user_story_tab()
 
-with left_column:    
-    with st.container(border=border, height=1200):
+with left_column:
+    with st.container(border=border, height=1100):
         # conversation
         for message in st.session_state.chat_history:
             if isinstance(message, AIMessage):
@@ -128,9 +135,10 @@ with left_column:
                     st.write(message.content)
 
         # user input
-        is_interactive = st.checkbox("交互对话模式", value=True)
         user_query = ''
         with st.container():
+            is_interactive = st.checkbox("交互对话模式", value=False)
+
             user_query = st.chat_input("What is up?")
             button_b_pos = "0rem"
             button_css = float_css_helper(width="2.2rem", bottom=button_b_pos, transition=0)
